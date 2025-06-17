6637f93f885e408d3dcb5eeaf93649f852659841
Sayandeep-cpu <sayandeepsaha70@gmail.com>
2025-06-17 22:36:49 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { v4 as uuid } from "uuid";
 
 function App() {
+
+  const [todo , setTodo] = useState('');
+  const [todoList , setTodoList] = useState([]);
+
+  const onTodoInputChange = (e) => {
+    setTodo(e.target.value);
+  }
+  const onAddTodoClick = () => {
+    setTodoList([...todoList, {id: uuid(), todo: todo , isCompleted: false}]);
+    setTodo('');
+  }
+  const onDeleteClick = (id) =>{
+    const updatedTodoList = todoList.filter(todo => todo.id !== id);
+    setTodoList(updatedTodoList);
+  }
+  const onTodoCheckChange = (id) => {
+    const updatedTodoList = todoList.map(todo => 
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
+    setTodoList(updatedTodoList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='APP'>
+    <h1> MY WISHLIST </h1>
+    <div>
+      <input value={todo} onChange={onTodoInputChange} placeholder='ADD YOUR WISHLIST HERE' />
+      {/* here we have created a input field , onChange is the eventListener here */}
+      <button onClick={onAddTodoClick}>ADD</button>
     </div>
+    <div>
+      {
+       todoList?.length > 0 && todoList.map(todo => (
+        <div key={todo.id}>
+          <label>
+           <input
+             onChange={() => onTodoCheckChange(todo.id)}
+             type='checkbox'
+             checked={todo.isCompleted}
+           />
+           <span className={todo.isCompleted ? 'strike-through' : ''}>{todo.todo}</span>
+          </label>
+          <button onClick={() => onDeleteClick(todo.id)}>DELETE</button>
+        </div>
+       ))
+      }
+    </div>
+   </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( //render means show something on the browser
   <React.StrictMode>
     <App />
   </React.StrictMode>
